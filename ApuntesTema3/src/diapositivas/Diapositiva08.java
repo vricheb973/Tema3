de5433998dbd7195b1f34b952ac64ed2138c9dfc
FranPerez <fperrei540@g.educaand.es>
2024-10-15 13:40:38 +0200

    Update Diapositiva08.java

--- ApuntesTema3/src/diapositivas/Diapositiva08.java
+++ ApuntesTema3/src/diapositivas/Diapositiva08.java
@@ -3,7 +3,16 @@ package diapositivas;
 public class Diapositiva08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int n = 0;
+		
+		while(n <= 50) {
+			if(n%2 == 0) {
+				System.out.println(n);
+			}
+			
+			n++;
+		}
 
 	}
 
