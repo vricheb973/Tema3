7c594f9b13bddb7bb8e709ff0f9891f9a7f26c97
FranPerez <vricheb973@g.educaand.es>
2024-11-05 13:38:47 +0100

    Añadidas actividades y ejemplos

---
+++ ApuntesTema3/src/apuntes/TablasMultiplicar.java
@@ -0,0 +1,28 @@
+package apuntes;
+
+public class TablasMultiplicar {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=10; i++) {
+			System.out.printf("Tabla del %d \n", i);
+			for(int j=1; j<=10; j++) {
+				System.out.printf("%d X %d = %d \n", i, j, i*j);
+			}
+			System.out.println("-----------------");
+		}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+
+	}
+
+}
---
+++ ApuntesTema3/src/boletin/Actividad18.java
@@ -0,0 +1,32 @@
+package boletin;
+
+import java.util.Scanner;
+
+public class Actividad18 {
+
+	public static void main(String[] args) {
+
+		Scanner sca = new Scanner(System.in);
+		
+		System.out.println("Dime un número: ");
+		int numero = sca.nextInt();
+		
+		String palotes = "";
+		
+		for(int i=numero; i>0; i=i/10) {
+			int digito = i%10;
+			
+			for(int j=0; j<digito; j++) {
+				palotes = "|" + palotes;
+			}
+			if(i>=10) {
+				palotes = "-" + palotes;
+			}
+		}
+		
+		System.out.println(palotes);
+		
+
+	}
+
+}
